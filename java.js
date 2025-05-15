4fc07da85be1c739be8ada2a53b0b9abf6c3af8d
helloizy <helloizy.zamboski@escola.pr.gov.br>
2025-05-15 11:41:57 -0300

    Create java.js

---
+++ java.js
@@ -0,0 +1,22 @@
+/* Dark mode styles */
+body.dark-mode {
+  background: #1e1e1e;
+  color: #ddd;
+}
+body.dark-mode header {
+  background: #333;
+  color: #4caf50;
+  box-shadow: 0 4px 8px rgba(255 255 255 / 0.1);
+}
+body.dark-mode main section {
+  background: #2b2b2b;
+  box-shadow: 0 2px 12px rgb(255 255 255 / 0.1);
+  color: #eee;
+}
+body.dark-mode footer {
+  color: #999;
+}
+body.dark-mode button {
+  border-color: #eee;
+  color: #eee;
+}
